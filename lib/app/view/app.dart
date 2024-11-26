a31c3baa32126dc82e1a66094bbf551ba11c337b
pietroid <pietrodomingues@gmail.com>
2024-11-25 21:24:24 -0300

    adding app_router

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,4 @@
-import 'package:cron/counter/counter.dart';
-import 'package:cron/l10n/l10n.dart';
+import 'package:cron/routing/app_router.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -7,16 +6,14 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: AppRouter().router,
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         ),
         useMaterial3: true,
       ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
     );
   }
 }
