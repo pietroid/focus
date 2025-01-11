72c5ce18547662681aef7622f68c19b3204da9db
pietroid <pietrodomingues@gmail.com>
2025-01-11 18:17:50 -0300

    removing unnecessary code

--- lib/app/core/home/core/home_screen.dart
+++ lib/app/core/home/core/home_screen.dart
@@ -1,24 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:focus/app/core/home/core/sections/timely/timely.dart';
-import 'package:focus/app/ui/creation_bottom_sheet.dart';
 import 'package:focus/app/ui/elements/global_scaffold.dart';
-import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final creationBottomSheet = context.read<CreationBottomSheet>();
     return const GlobalScaffold(
-      // floatingActionButton: FloatingActionButton(
-      //   backgroundColor: const Color.fromARGB(255, 0, 125, 187),
-      //   onPressed: () {
-      //     creationBottomSheet.show(context);
-      //   },
-      //   shape: const CircleBorder(),
-      //   child: const Icon(Icons.add, color: Colors.black),
-      // ),
       child: Timely(),
     );
   }
