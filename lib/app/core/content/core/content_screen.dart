e01a00f32fa57a2b78c0516a48b0a70a3fe73d0b
Pietro Teruya <pietroteruya@Pietros-MacBook-Pro.local>
2025-01-22 11:04:54 -0300

    adding todo

--- lib/app/core/content/core/content_screen.dart
+++ lib/app/core/content/core/content_screen.dart
@@ -29,6 +29,7 @@ class ContentScreen extends StatelessWidget {
         keyForList: (thing) => ValueKey(thing.id),
         itemsForList: (list) => list.children,
         listHeader: (list) => ContentHeader(
+          //TODO: add mapper
           ContentHeaderParams(title: list.content),
         ),
         keyForItem: (item) => ValueKey(item.id),
