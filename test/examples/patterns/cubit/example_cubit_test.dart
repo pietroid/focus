faeb088e6bc8c1a806b90990301a0cdf221cca90
pietroid <pietrodomingues@gmail.com>
2025-03-03 10:56:04 -0300

    renaming folder

--- test/examples/patterns/cubit/example_cubit_test.dart
+++ test/examples/patterns/cubit/example_cubit_test.dart
@@ -1,6 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:focus/examples/cubit/example_cubit.dart';
+import 'package:focus/examples/patterns/cubit/example_cubit.dart';
 
 void main() {
   group('ExampleCubit', () {
