e9f64dd52de366bc3db1f85e187a246c1c588a77
pietroid <pietrodomingues@gmail.com>
2025-03-16 08:30:44 -0300

    first magic of time!

--- packages/timely_repository/lib/src/timely_repository.dart
+++ packages/timely_repository/lib/src/timely_repository.dart
@@ -2,6 +2,8 @@ import 'package:local_service/local_service.dart';
 import 'package:rxdart/subjects.dart';
 import 'package:things/things.dart';
 
+const intervalBetweenThings = Duration(minutes: 5);
+
 class TimelyRepository {
   TimelyRepository({
     required this.box,
@@ -26,4 +28,60 @@ class TimelyRepository {
       },
     ).behaviorSubject;
   }
+
+  void listenToDurationChanges() {
+    List<Thing> latestItems = [];
+    stream.listen((nowAndTodayThings) {
+      final currentItems = [
+        ...nowAndTodayThings.first.children.toList(),
+        ...nowAndTodayThings[1].children.toList()
+      ];
+
+      bool equalList = true;
+      if (currentItems.length == latestItems.length) {
+        for (int i = 0; i < currentItems.length; i++) {
+          if (!(currentItems[i].duration == latestItems[i].duration &&
+              isAlmostEqualOrNull(
+                  currentItems[i].startTime, latestItems[i].startTime) &&
+              isAlmostEqualOrNull(
+                  currentItems[i].endTime, latestItems[i].endTime))) {
+            equalList = false;
+            break;
+          }
+        }
+      } else {
+        equalList = false;
+      }
+
+      if (equalList) return;
+
+      DateTime currentInitialTime = DateTime.now();
+
+      for (final item in currentItems) {
+        item.startTime = currentInitialTime;
+        box.store.box<Thing>().put(item);
+
+        currentInitialTime =
+            currentInitialTime.add(item.duration ?? Duration.zero);
+        currentInitialTime = currentInitialTime.add(intervalBetweenThings);
+      }
+
+      latestItems = currentItems.toList();
+
+      // for (final nowAndTodayThing in nowAndTodayThings) {
+      //   nowAndTodayThing.startTime = currentInitialTime;
+      //   box.store.box<Thing>().put(nowAndTodayThing);
+
+      //   currentInitialTime =
+      //       currentInitialTime.add(nowAndTodayThing.duration ?? Duration.zero);
+      //   currentInitialTime = currentInitialTime.add(intervalBetweenThings);
+      // }
+    });
+  }
+}
+
+bool isAlmostEqualOrNull(DateTime? a, DateTime? b) {
+  if (a == null && b == null) return true;
+  if (a == null || b == null) return false;
+  return a.difference(b).inSeconds < 10;
 }
